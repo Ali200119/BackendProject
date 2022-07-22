ff2c001bab2a8914c96422f327bb6fd0abe16e6f
Ali200119 <aliit@code.edu.az>
2022-07-22 20:10:26 +0400

    bug-fix: Title

--- Allup/Startup.cs
+++ Allup/Startup.cs
@@ -13,13 +13,6 @@ namespace Allup
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -33,12 +26,7 @@ namespace Allup
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
