e1e163970c056f6954fae954bf9e8bfb6540e159
Ali200119 <aliit@code.edu.az>
2022-08-03 20:15:31 +0400

    feat: Product CRUD

---
+++ Allup/Areas/AdminPanel/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Allup.DAL;
+using Allup.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Allup.Areas.AdminPanel.Controllers
+{
+    [Area(nameof(AdminPanel))]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products.Include(p => p.Category).Include(p => p.Brand).ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
